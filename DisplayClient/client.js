e47a5a8afbd71c44c878a528fe406a9942dcc661
Simon Ldj <simon.ldj@gmail.com>
2016-04-06 12:13:32 +0300

    Refactor client to log data
    Log data (append) in client
    instead of displaying only the last message.

--- DisplayClient/client.js
+++ DisplayClient/client.js
@@ -1,18 +1,34 @@
 console.log("start");
-$('#container').html("start");
 
-// creating a new websocket
-var socket = io.connect('http://localhost:7000');
+var mainOutput = $('#output');
 
-// on every message received we print the new data inside the #container div
-socket.on('notification', function (data) {
-    console.log("new data from socket: " + data);
+function initialize(){
+    mainOutput.find('time').html('Last Update: never');
+}
+initialize();
 
-    // convert the json string into a valid javascript object
-    //var _data = JSON.parse(data);
 
-    //$('#container').html(_data.test.sample);
-    $('#container').html("data: " + data);
-    $('time').html('Last Update:' + new Date());
-});
+function connect(){
+    // creating a new websocket
+    var socket = io.connect('http://localhost:7000');
 
+    // on every message received we print the new data inside the #container div
+    socket.on('notification', function (data) {
+        console.log("new data from socket: " + data);
+
+        // convert the json string into a valid javascript object
+        //var _data = JSON.parse(data);
+
+        //$('#container').html(_data.test.sample);
+        addData(data);
+    });
+
+};
+connect();
+
+// add data to the DOM
+function addData(data){
+
+    mainOutput.find('#content').append("<br>" + data);
+    mainOutput.find('time').html('Last Update:' + new Date());
+}
