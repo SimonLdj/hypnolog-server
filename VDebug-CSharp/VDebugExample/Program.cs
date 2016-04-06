db351407b27c2369b0a84790a9dba90db7763ab0
Simon Ldj <simon.ldj@gmail.com>
2016-04-06 15:48:34 +0300

    Implement VDebug-CSharp
    Implement C# VDebug input client (library - dll)
    and example project to test the library.

---
+++ VDebug-CSharp/VDebugExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VDebugLib;
+
+namespace VDebugExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start example");
+            VDebug.Init();
+
+            VDebug.Log("Example 1");
+            VDebug.Log("Example 2");
+            VDebug.Log("Example 3");
+            VDebug.Log(new
+            {
+                Name = "Example",
+                Value = new
+                {
+                    VarA = 4,
+                    varB = 9
+                }
+            });
+        }
+    }
+}
---
+++ VDebug-CSharp/VDebugLib/VDebug.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDebugLib
+{
+    public class VDebug
+    {
+
+        static private readonly Uri baseAddress = new Uri("http://localhost:7000/in");
+
+        static VDebug()
+        {
+        }
+
+        public static void Init()
+        {
+            Log("New session");
+        }
+
+        public static void Log(object obj)
+        {
+            var wait = LogAsync(obj).Result;
+        }
+
+        public static async Task<string> LogAsync(object obj)
+        {
+            // create HTTP Client and send HTTP post request
+            var client = new HttpClient();
+            var result = client.PostAsJsonAsync(baseAddress.ToString(), obj).Result;
+
+            string resultContent = result.Content.ReadAsStringAsync().Result;
+
+            // Check that response was successful or throw exception
+            //response.EnsureSuccessStatusCode();
+
+            // Read result as Contact
+            //Contact result = await response.Content.ReadAsAsync<Contact>();
+
+            return resultContent;
+        }
+
+    }
+}
