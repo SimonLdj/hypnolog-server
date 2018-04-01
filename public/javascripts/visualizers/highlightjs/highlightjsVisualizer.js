a0ecb53215cd03d95484fb1d152c917c2ff2999e
Simon Ldj <simon.ldj@gmail.com>
2018-04-01 19:58:19 +0300

    Add highlight.js visualizer

---
+++ public/javascripts/visualizers/highlightjs/highlightjsVisualizer.js
@@ -0,0 +1,42 @@
+// Code Hight Light Visualizer
+//
+// uses highlight.js (https://highlightjs.org/) to display Syntax highlighted code.
+//
+//
+'use strict';
+let HighlightjsVisualizer = (function() {
+
+    let displayTypes = ["code", "json", "html"];
+    let exports = {};
+
+    // public functions:
+
+    exports.canDisplay = function(obj){
+        // Check if we support given object type
+        if (displayTypes.some(s => s === obj.type.toLocaleLowerCase()))
+            return true;
+        return false;
+    }
+
+    exports.display = function(obj, callback){
+        // return false if we can not visualize the object
+        if (!exports.canDisplay(obj))
+            return false;
+
+        // creating new <pre> DOM elements
+        let element = document.createElement("pre");
+        element.appendChild(document.createTextNode((JSON.stringify(obj.data, null, "\t"))));
+
+        // let highlight.js do its work
+        hljs.highlightBlock(element);
+
+        // pass the new element to the callback
+        callback(element);
+
+        // return true, as yes, visualization was done successfully
+        return true;
+    }
+
+    return exports;
+
+})();
